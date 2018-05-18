4932220c077b8e6ab125bacb907b776074a2f7a7
Aaron Tolman <tolman-aaron@zai.com>
2018-05-18 08:06:41 -0400

    Fix path to nomad_version variable

--- nomad_alt/__init__.py
+++ nomad_alt/__init__.py
@@ -1,6 +1,6 @@
 from nomad_alt.exceptions import NomadException, ACLDisabled, ACLPermissionDenied, NotFound, BadRequest
 
-from nomad_version import __version__
+from nomad_alt.nomad_version import __version__
 
 from nomad_alt.base import Check
 from nomad_alt.std import Nomad
